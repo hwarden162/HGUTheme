7f86f2e5c4d90925bf87902642081b7c7216c1a5
Hugh Warden <hugh.warden@outlook.com>
2022-08-20 16:05:53 +0100

    Setting Up Documentation

--- R/hgu_colours.R
+++ R/hgu_colours.R
@@ -1,4 +1,59 @@
+# Colour Gradient Code ----------------------------------------------------
 
-# Add colours code here ---------------------------------------------------
+#' Scale Colours With A Gradient
+#'
+#' @param ...
+#' @param low
+#' @param high
+#' @param space
+#' @param na.value
+#' @param guide
+#' @param aesthetics
+#'
+#' @return
+#' @export
+#'
+#' @examples
+scale_colour_gradient_hgu <- function(
+    ...,
+    low = "#2D2E5F",
+    high = "#54BBD2",
+    space = "Lab",
+    na.value = "grey50",
+    guide = "colourbar",
+    aesthetics = "colour"
+) {
+  ggplot2::scale_colour_gradient(
+    ...,
+    low = low,
+    high = high,
+    space = space,
+    na.value = na.value,
+    guide = guide,
+    aesthetics = aesthetics
+  )
+}
 
-
+scale_colour_gradient2_hgu <- function(
+    ...,
+    low = "#2D2E5F",
+    mid = "#3A88A9",
+    high = "#54BBD2",
+    midpoint = 0,
+    space = "Lab",
+    na.value = "grey50",
+    guide = "colourbar",
+    aesthetics = "colour"
+) {
+  ggplot2::scale_colour_gradient2(
+    ...,
+    low = low,
+    mid = mid,
+    high = high,
+    midpoint = midpoint,
+    space = space,
+    na.value = na.value,
+    guide = guide,
+    aesthetics = aesthetics
+  )
+}
