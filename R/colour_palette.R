52f11e9c58ae712e6ce4f625df9af86e23dd6f93
Hugh Warden <hugh.warden@outlook.com>
2022-08-21 14:59:27 +0100

    Adding MRC colours

--- R/colour_palette.R
+++ R/colour_palette.R
@@ -49,3 +49,102 @@ hgu_palette <- function() {
     hgu_light()
   )
 }
+
+#' Hex Value Of MRC Dark Blue
+#'
+#' @return A character string of the hex value of the MRC dark blue.
+#' @export
+#'
+#' @examples
+#' colour <- mrc_dark_blue()
+#' colour
+mrc_dark_blue <- function() {
+  "#2E2C5E"
+}
+
+#' Hex Value Of MRC Darkish Blue
+#'
+#' @return A character string of the hex value of the MRC darkish blue.
+#' @export
+#'
+#' @examples
+#' colour <- mrc_darkish_blue()
+#' colour
+mrc_darkish_blue <- function() {
+  "#102F83"
+}
+
+#' Hex Value Of MRC Mid Blue
+#'
+#' @return A character string of the hex value of the MRC mid blue.
+#' @export
+#'
+#' @examples
+#' colour <- mrc_mid_blue()
+#' colour
+mrc_mid_blue <- function() {
+  "#305CEF"
+}
+
+#' Hex Value Of MRC Red
+#'
+#' @return A character string of the hex value of the MRC red.
+#' @export
+#'
+#' @examples
+#' colour <- mrc_red()
+#' colour
+mrc_red <- function() {
+  "#D85941"
+}
+
+#' Hex Value Of MRC Green
+#'
+#' @return A character string of the hex value of the MRC green.
+#' @export
+#'
+#' @examples
+#' colour <- mrc_green()
+#' colour
+mrc_green <- function() {
+  "#4AA789"
+}
+
+#' Hex Value Of MRC Yellow
+#'
+#' @return A character string of the hex value of the MRC yellow.
+#' @export
+#'
+#' @examples
+#' colour <- mrc_yellow()
+#' colour
+mrc_yellow <- function() {
+  "#F1BE44"
+}
+
+#' Hex Value Of MRC Grey
+#'
+#' @return A character string of the hex value of the MRC grey.
+#' @export
+#'
+#' @examples
+#' colour <- mrc_grey()
+#' colour
+mrc_grey <- function() {
+  "#DADADA"
+}
+
+#' Hex Value Of MRC Gray
+#'
+#' @return A character string of the hex value of the MRC gray.
+#' @export
+#'
+#' @examples
+#' colour <- mrc_gray()
+#' colour
+mrc_gray <- function() {
+  mrc_grey()
+}
+
+
+
