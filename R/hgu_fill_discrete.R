833941e1e24a41450332839a362cce14510d3700
Hugh Warden <hugh.warden@outlook.com>
2022-08-21 17:11:54 +0100

    Adding HGU fill code

---
+++ R/hgu_fill_discrete.R
@@ -0,0 +1,196 @@
+#' Assign Discrete HGU 3 Colour Fill Palette
+#'
+#' @param ... Arguments to be passed on.
+#' @param values A character vector of colour values to map data to.
+#' @param aesthetics The aesthetic to apply this scale to.
+#' @param breaks Values for beaks (see `ggplot2::scale_colour_manual` for details).
+#' @param na.value Colour to be used for missing values.
+#'
+#' @return A layer that can be added to a ggplot2 object.
+#' @export
+#'
+#' @examples
+#' library(tidyverse)
+#' tibble(
+#'   x = rnorm(30),
+#'   y = runif(30)
+#' ) %>%
+#'   ggplot(aes(x=x,y=y)) +
+#'   geom_density2d_filled(bins = 3) +
+#'   theme_classic() +
+#'   scale_fill_hgu3()
+scale_fill_hgu3 <- function(
+    ...,
+    values = hgu_palette(),
+    aesthetics = "fill",
+    breaks = ggplot2::waiver(),
+    na.value = mrc_grey()
+) {
+  ggplot2::scale_fill_manual(
+    ...,
+    values = values,
+    aesthetics = aesthetics,
+    breaks = breaks,
+    na.value = na.value
+  )
+}
+
+#' Assign Discrete HGU 2 Colour Fill Palette
+#'
+#' @param ... Arguments to be passed on.
+#' @param values A character vector of colour values to map data to.
+#' @param aesthetics The aesthetic to apply this scale to.
+#' @param breaks Values for beaks (see `ggplot2::scale_colour_manual` for details).
+#' @param na.value Colour to be used for missing values.
+#'
+#' @return A layer that can be added to a ggplot2 object.
+#' @export
+#'
+#' @examples
+#' library(tidyverse)
+#' tibble(
+#'   x = rnorm(30),
+#'   y = runif(30)
+#' ) %>%
+#'   ggplot(aes(x=x,y=y)) +
+#'   geom_density2d_filled(bins = 2) +
+#'   theme_classic() +
+#'   scale_fill_hgu2_low_high()
+scale_fill_hgu2_low_high <- function(
+    ...,
+    values = c(
+      hgu_dark(),
+      hgu_light()
+    ),
+    aesthetics = "fill",
+    breaks = ggplot2::waiver(),
+    na.value = mrc_grey()
+) {
+  ggplot2::scale_fill_manual(
+    ...,
+    values = values,
+    aesthetics = aesthetics,
+    breaks = breaks,
+    na.value = na.value
+  )
+}
+
+#' Assign Discrete HGU 2 Colour Fill Palette
+#'
+#' @param ... Arguments to be passed on.
+#' @param values A character vector of colour values to map data to.
+#' @param aesthetics The aesthetic to apply this scale to.
+#' @param breaks Values for beaks (see `ggplot2::scale_colour_manual` for details).
+#' @param na.value Colour to be used for missing values.
+#'
+#' @return A layer that can be added to a ggplot2 object.
+#' @export
+#'
+#' @examples
+#' library(tidyverse)
+#' tibble(
+#'   x = rnorm(30),
+#'   y = runif(30)
+#' ) %>%
+#'   ggplot(aes(x=x,y=y)) +
+#'   geom_density2d_filled(bins = 2) +
+#'   theme_classic() +
+#'   scale_fill_hgu2_mid_high()
+scale_fill_hgu2_mid_high <- function(
+    ...,
+    values = c(
+      hgu_mid(),
+      hgu_light()
+    ),
+    aesthetics = "fill",
+    breaks = ggplot2::waiver(),
+    na.value = mrc_grey()
+) {
+  ggplot2::scale_fill_manual(
+    ...,
+    values = values,
+    aesthetics = aesthetics,
+    breaks = breaks,
+    na.value = na.value
+  )
+}
+
+#' Assign Discrete HGU 2 Colour Fill Palette
+#'
+#' @param ... Arguments to be passed on.
+#' @param values A character vector of colour values to map data to.
+#' @param aesthetics The aesthetic to apply this scale to.
+#' @param breaks Values for beaks (see `ggplot2::scale_colour_manual` for details).
+#' @param na.value Colour to be used for missing values.
+#'
+#' @return A layer that can be added to a ggplot2 object.
+#' @export
+#'
+#' @examples
+#' library(tidyverse)
+#' tibble(
+#'   x = rnorm(30),
+#'   y = runif(30)
+#' ) %>%
+#'   ggplot(aes(x=x,y=y)) +
+#'   geom_density2d_filled(bins = 2) +
+#'   theme_classic() +
+#'   scale_fill_hgu2_low_mid()
+scale_fill_hgu2_low_mid <- function(
+    ...,
+    values = c(
+      hgu_dark(),
+      hgu_mid()
+    ),
+    aesthetics = "fill",
+    breaks = ggplot2::waiver(),
+    na.value = mrc_grey()
+) {
+  ggplot2::scale_fill_manual(
+    ...,
+    values = values,
+    aesthetics = aesthetics,
+    breaks = breaks,
+    na.value = na.value
+  )
+}
+
+#' Assign Discrete HGU 2 Colour Fill Palette
+#'
+#' @param ... Arguments to be passed on.
+#' @param values A character vector of colour values to map data to.
+#' @param aesthetics The aesthetic to apply this scale to.
+#' @param breaks Values for beaks (see `ggplot2::scale_colour_manual` for details).
+#' @param na.value Colour to be used for missing values.
+#'
+#' @return A layer that can be added to a ggplot2 object.
+#' @export
+#'
+#' @examples
+#' library(tidyverse)
+#' tibble(
+#'   x = rnorm(30),
+#'   y = runif(30)
+#' ) %>%
+#'   ggplot(aes(x=x,y=y)) +
+#'   geom_density2d_filled(bins = 2) +
+#'   theme_classic() +
+#'   scale_fill_hgu2()
+scale_fill_hgu2 <- function(
+    ...,
+    values = c(
+      hgu_dark(),
+      hgu_light()
+    ),
+    aesthetics = "fill",
+    breaks = ggplot2::waiver(),
+    na.value = mrc_grey()
+) {
+  ggplot2::scale_fill_manual(
+    ...,
+    values = values,
+    aesthetics = aesthetics,
+    breaks = breaks,
+    na.value = na.value
+  )
+}
---
+++ R/hgu_fill_gradient.R
@@ -0,0 +1,89 @@
+#' Scale Fill With A Gradient
+#'
+#' @param ... Arguments to be passed on.
+#' @param low Colour for the low end of the gradient.
+#' @param high Colour for the high end of the gradient.
+#' @param space Colour space in which to calculate the gradient.
+#' @param na.value Colour to be used for missing values.
+#' @param guide Type of legend.
+#' @param aesthetics The aesthetic to apply this scale to.
+#'
+#' @return A layer that can be added to a ggplot2 object.
+#' @export
+#'
+#' @examples
+#' library(tidyverse)
+#' tibble(
+#'   x = rnorm(500000),
+#'   y = rnorm(500000)
+#' ) %>%
+#'   ggplot(aes(x = x, y = y)) +
+#'   geom_hex(bins = 40) + theme_classic() +
+#'   scale_fill_gradient_hgu()
+scale_fill_gradient_hgu <- function(
+    ...,
+    low = hgu_dark(),
+    high = hgu_light(),
+    space = "Lab",
+    na.value = mrc_grey(),
+    guide = "colourbar",
+    aesthetics = "fill"
+) {
+  ggplot2::scale_fill_gradient(
+    ...,
+    low = low,
+    high = high,
+    space = space,
+    na.value = na.value,
+    guide = guide,
+    aesthetics = aesthetics
+  )
+}
+
+#' Scale Fill With A Gradient
+#'
+#' @param ... Arguments to be passed on.
+#' @param low Colour for the low end of the gradient.
+#' @param mid Colour for the midpoit of the gradient.
+#' @param high Colour for the high end of the gradient.
+#' @param midpoint The midpoint (in data value) of the diverging scale. Defaults to 0.
+#' @param space Colour space in which to calculate the gradient.
+#' @param na.value Colour to be used for missing values.
+#' @param guide Type of legend.
+#' @param aesthetics The aesthetic to apply this scale to.
+#'
+#' @return A layer that can be added to a ggplot2 object.
+#' @export
+#'
+#' @examples
+#' library(tidyverse)
+#' tibble(
+#'   x = rnorm(500000),
+#'   y = rnorm(500000)
+#' ) %>%
+#'   ggplot(aes(x = x, y = y)) +
+#'   geom_hex(bins = 40) + theme_classic() +
+#'   scale_fill_gradient2_hgu(midpoint = 1600)
+scale_fill_gradient2_hgu <- function(
+    ...,
+    low = hgu_dark(),
+    mid = hgu_mid(),
+    high = hgu_light(),
+    midpoint = 0,
+    space = "Lab",
+    na.value = mrc_grey(),
+    guide = "colourbar",
+    aesthetics = "fill"
+){
+  ggplot2::scale_fill_gradient2(
+    ...,
+    low = low,
+    mid = mid,
+    high = high,
+    midpoint = midpoint,
+    space = space,
+    na.value = na.value,
+    guide = guide,
+    aesthetics = aesthetics
+  )
+}
