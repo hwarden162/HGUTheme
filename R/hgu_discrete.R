fd70b1e34453af0ce0224867f6cf1f5209d6c437
Hugh Warden <hugh.warden@outlook.com>
2022-08-21 16:08:40 +0100

    Adding 2 colour HGU discrete colours

--- R/hgu_discrete.R
+++ R/hgu_discrete.R
@@ -18,18 +18,14 @@
 #' ) %>%
 #'   ggplot(aes(x = x, y = y, colour = col)) +
 #'   geom_point() +
-#'   scale_colour_hgu_3col() +
+#'   scale_colour_hgu3() +
 #'   theme_classic()
-scale_colour_hgu_3col <- function(
+scale_colour_hgu3 <- function(
     ...,
-    values = c(
-      hgu_dark(),
-      hgu_mid(),
-      hgu_light()
-    ),
+    values = hgu_palette(),
     aesthetics = "colour",
     breaks = ggplot2::waiver(),
-    na.value = "grey50"
+    na.value = mrc_grey()
 ) {
   ggplot2::scale_colour_manual(
     ...,
@@ -39,3 +35,170 @@ scale_colour_hgu_3col <- function(
     na.value = na.value
   )
 }
+
+#' Assign Discrete HGU 2 Colour Palette
+#'
+#' @param ... Arguments to be passed on.
+#' @param values A character vector of colour values to map data to.
+#' @param aesthetics The aesthetic to apply this scale to.
+#' @param breaks Values for beaks (see `ggplot2::scale_colour_manual` for details).
+#' @param na.value Colour to be used for missing values.
+#'
+#' @return A layer that can be added to a ggplot2 object.
+#' @export
+#'
+#' @examples
+#' library(tidyverse)
+#' tibble(
+#'   x = rnorm(60),
+#'   y = rnorm(60),
+#'   col = rep(c("a", "b"), 30)
+#' ) %>%
+#'   ggplot(aes(x = x, y = y, colour = col)) +
+#'   geom_point() +
+#'   scale_colour_hgu2_low_high() +
+#'   theme_classic()
+scale_colour_hgu2_low_high <- function(
+    ...,
+    values = c(
+      hgu_dark(),
+      hgu_light()
+    ),
+    aesthetics = "colour",
+    breaks = ggplot2::waiver(),
+    na.value = mrc_grey()
+) {
+  ggplot2::scale_colour_manual(
+    ...,
+    values = values,
+    aesthetics = aesthetics,
+    breaks = breaks,
+    na.value = na.value
+  )
+}
+
+#' Assign Discrete HGU 2 Colour Palette
+#'
+#' @param ... Arguments to be passed on.
+#' @param values A character vector of colour values to map data to.
+#' @param aesthetics The aesthetic to apply this scale to.
+#' @param breaks Values for beaks (see `ggplot2::scale_colour_manual` for details).
+#' @param na.value Colour to be used for missing values.
+#'
+#' @return A layer that can be added to a ggplot2 object.
+#' @export
+#'
+#' @examples
+#' library(tidyverse)
+#' tibble(
+#'   x = rnorm(60),
+#'   y = rnorm(60),
+#'   col = rep(c("a", "b"), 30)
+#' ) %>%
+#'   ggplot(aes(x = x, y = y, colour = col)) +
+#'   geom_point() +
+#'   scale_colour_hgu2_low_mid() +
+#'   theme_classic()
+scale_colour_hgu2_low_mid <- function(
+    ...,
+    values = c(
+      hgu_dark(),
+      hgu_mid()
+    ),
+    aesthetics = "colour",
+    breaks = ggplot2::waiver(),
+    na.value = mrc_grey()
+) {
+  ggplot2::scale_colour_manual(
+    ...,
+    values = values,
+    aesthetics = aesthetics,
+    breaks = breaks,
+    na.value = na.value
+  )
+}
+
+
+#' Assign Discrete HGU 2 Colour Palette
+#'
+#' @param ... Arguments to be passed on.
+#' @param values A character vector of colour values to map data to.
+#' @param aesthetics The aesthetic to apply this scale to.
+#' @param breaks Values for beaks (see `ggplot2::scale_colour_manual` for details).
+#' @param na.value Colour to be used for missing values.
+#'
+#' @return A layer that can be added to a ggplot2 object.
+#' @export
+#'
+#' @examples
+#' library(tidyverse)
+#' tibble(
+#'   x = rnorm(60),
+#'   y = rnorm(60),
+#'   col = rep(c("a", "b"), 30)
+#' ) %>%
+#'   ggplot(aes(x = x, y = y, colour = col)) +
+#'   geom_point() +
+#'   scale_colour_hgu2_mid_high() +
+#'   theme_classic()
+scale_colour_hgu2_mid_high <- function(
+    ...,
+    values = c(
+      hgu_mid(),
+      hgu_light()
+    ),
+    aesthetics = "colour",
+    breaks = ggplot2::waiver(),
+    na.value = mrc_grey()
+) {
+  ggplot2::scale_colour_manual(
+    ...,
+    values = values,
+    aesthetics = aesthetics,
+    breaks = breaks,
+    na.value = na.value
+  )
+}
+
+#' Assign Discrete HGU 2 Colour Palette
+#'
+#' @param ... Arguments to be passed on.
+#' @param values A character vector of colour values to map data to.
+#' @param aesthetics The aesthetic to apply this scale to.
+#' @param breaks Values for beaks (see `ggplot2::scale_colour_manual` for details).
+#' @param na.value Colour to be used for missing values.
+#'
+#' @return A layer that can be added to a ggplot2 object.
+#' @export
+#'
+#' @examples
+#' library(tidyverse)
+#' tibble(
+#'   x = rnorm(60),
+#'   y = rnorm(60),
+#'   col = rep(c("a", "b"), 30)
+#' ) %>%
+#'   ggplot(aes(x = x, y = y, colour = col)) +
+#'   geom_point() +
+#'   scale_colour_hgu2() +
+#'   theme_classic()
+scale_colour_hgu2 <- function(
+    ...,
+    values = c(
+      hgu_dark(),
+      hgu_light()
+    ),
+    aesthetics = "colour",
+    breaks = ggplot2::waiver(),
+    na.value = mrc_grey()
+) {
+  ggplot2::scale_colour_manual(
+    ...,
+    values = values,
+    aesthetics = aesthetics,
+    breaks = breaks,
+    na.value = na.value
+  )
+}
+
+
