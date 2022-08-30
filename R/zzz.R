8265675a98d31f1326e43692498ce03986e79df4
Hugh Warden <hugh.warden@outlook.com>
2022-08-30 13:33:07 +0100

    Adding startup message

---
+++ R/zzz.R
@@ -0,0 +1,12 @@
+.onLoad <- function(libname, pkgname) {
+  packageStartupMessage(
+    "--HGUTheme-----------------------------------------------------------------\n",
+    "Please be mindful of colourblind users when colouring your plots.\n",
+    "More information on how to use HGUTheme at:\n",
+    "https://hwarden162.github.io/HGUTheme/\n",
+    "---------------------------------------------------------------------------"
+  )
+}
+
+
+
