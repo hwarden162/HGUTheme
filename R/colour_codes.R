4040de78d0dc0f67775f1beb4e61098a07f8d187
Hugh Warden <hugh.warden@outlook.com>
2023-01-22 00:04:47 +0000

    Adding UKRI blues

---
+++ R/colour_codes.R
@@ -0,0 +1,56 @@
+
+# UKRI Colours ------------------------------------------------------------
+
+#' HEX Code of the UKRI Dark Blue
+#'
+#' Access the HEX code of the dark blue used in the UKRI logo.
+#'
+#' @return The HEX code of the UKRI dark blue.
+#' @export
+#'
+#' @examples
+#'
+#' barplot(
+#'   1:3,
+#'   col = ukri_dark_blue()
+#' )
+#'
+ukri_dark_blue <- function() {
+  "#2D2E5F"
+}
+
+#' HEX Code of the UKRI Mid Blue
+#'
+#' Access the HEX code of the mid blue used in the UKRI logo.
+#'
+#' @return The HEX code of the UKRI mid blue.
+#' @export
+#'
+#' @examples
+#'
+#' barplot(
+#'   1:3,
+#'   col = ukri_mid_blue()
+#' )
+#'
+ukri_mid_blue <- function() {
+  "#3A88A9"
+}
+
+#' HEX Code of the UKRI Light Blue
+#'
+#' Access the HEX code of the light blue used in the UKRI logo.
+#'
+#' @return The HEX code of the UKRI light blue.
+#' @export
+#'
+#' @examples
+#'
+#' barplot(
+#'   1:3,
+#'   col = ukri_light_blue()
+#' )
+#'
+ukri_light_blue <- function() {
+  "#00BAD2"
+}
