b1aafd0f17c326b2a08105c4277529dc9cb062b8
Hugh Warden <hugh.warden@outlook.com>
2023-01-22 13:54:11 +0000

    Adding CRUK colours

--- R/colour_codes.R
+++ R/colour_codes.R
@@ -72,3 +72,77 @@ ukri_light_blue <- function() {
 ukri_orange <- function() {
   "#F2BC44"
 }
+
+# CRUK Colours ------------------------------------------------------------
+
+#' HEX Code of the CRUK Dark Blue
+#'
+#' Access the HEX code of the dark blue used by CRUK.
+#'
+#' @return The HEX code of the CRUK dark blue.
+#' @export
+#'
+#' @examples
+#'
+#' barplot(
+#'   1:3,
+#'   col = cruk_dark_blue()
+#' )
+#'
+cruk_dark_blue <- function() {
+  "#2e0188"
+}
+
+#' HEX Code of the CRUK Light Blue
+#'
+#' Access the HEX code of the light blue used by CRUK.
+#'
+#' @return The HEX code of the CRUK light blue.
+#' @export
+#'
+#' @examples
+#'
+#' barplot(
+#'   1:3,
+#'   col = cruk_light_blue()
+#' )
+#'
+cruk_light_blue <- function() {
+  "#00b6ea"
+}
+
+#' HEX Code of the CRUK Pink
+#'
+#' Access the HEX code of the pink used by CRUK.
+#'
+#' @return The HEX code of the CRUK pink.
+#' @export
+#'
+#' @examples
+#'
+#' barplot(
+#'   1:3,
+#'   col = cruk_pink()
+#' )
+#'
+cruk_pink <- function() {
+  "#ee0286"
+}
+
+#' HEX Code of the CRUK Grey
+#'
+#' Access the HEX code of the grey used by CRUK.
+#'
+#' @return The HEX code of the CRUK grey.
+#' @export
+#'
+#' @examples
+#'
+#' barplot(
+#'   1:3,
+#'   col = cruk_grey()
+#' )
+#'
+cruk_grey <- function() {
+  "#c5c5c5"
+}
