fd57ef85f928bde6f200434d46b5e265cfb0f611
Hugh Warden <hugh.warden@outlook.com>
2023-01-22 13:56:39 +0000

    Documenting and testing

--- tests/testthat/test-colour_codes.R
+++ tests/testthat/test-colour_codes.R
@@ -16,3 +16,21 @@ test_that("UKRI light blue returned", {
 test_that("UKRI orange returned", {
   expect_equal(ukri_orange(), "#F2BC44")
 })
+
+# CRUK Colours ------------------------------------------------------------
+
+test_that("CRUK dark blue returned", {
+  expect_equal(cruk_dark_blue(), "#2e0188")
+})
+
+test_that("CRUK light blue returned", {
+  expect_equal(cruk_light_blue(), "#00b6ea")
+})
+
+test_that("CRUK pink returned", {
+  expect_equal(cruk_pink(), "#ee0286")
+})
+
+test_that("CRUK grey returned", {
+  expect_equal(cruk_grey(), "#c5c5c5")
+})
