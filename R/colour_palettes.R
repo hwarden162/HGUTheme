62dc4ed3129ba2886fd0143be04836637c8dc1f3
Hugh Warden <hugh.warden@outlook.com>
2023-01-22 23:31:15 +0000

    Adding CRUK colour palettes

--- R/colour_palettes.R
+++ R/colour_palettes.R
@@ -103,3 +103,108 @@ ukri_blues_n <- function(
   )
   func_gen(n)
 }
+
+# CRUK Colours ------------------------------------------------------------
+
+#' HEX Codes of the CRUK Colours
+#'
+#' Access the HEX codes of the colours used in the CRUK logo.
+#'
+#' @return The HEX codes of the CRUK colours.
+#' @export
+#'
+#' @examples
+#'
+#' barplot(
+#'   1:3,
+#'   col = cruk_colours()
+#' )
+#'
+cruk_colours <- function() {
+  c(
+    cruk_dark_blue(),
+    cruk_light_blue(),
+    cruk_pink()
+  )
+}
+
+#' Palette Generating Function For CRUK Colours
+#'
+#' @param colours The colours to interpolate between. By default these are set
+#'     to the blues from the UKRI logo.
+#' @param bias A positive number. Higher values will give more widely spaced
+#'     colours at the higher end of the colour spectrum.
+#' @param space A character string indicating whether the colours should be
+#'     interpolated in RGB or CIE Lab colour spaces.
+#' @param interpolate A character indicating whether a spline or linear
+#'     interpolation method should be used.
+#' @param alpha A logical value indicating whether an alpha channel should be
+#'     returned.
+#' @param ... Arguments to be passed to `grDevices::colorRampPalette`.
+#'
+#' @return A function to generate palettes that interpolate the CRUK colours.
+#'
+cruk_colours_palette_func_gen <- function(
+    colours = cruk_colours(),
+    bias = 1,
+    space = "Lab",
+    interpolate = "linear",
+    alpha = FALSE,
+    ...
+) {
+  grDevices::colorRampPalette(
+    colors = colours,
+    bias = bias,
+    space = space,
+    interpolate = interpolate,
+    alpha = alpha
+  )
+}
+
+#' Interpolated HEX Codes of the CRUK Colours
+#'
+#' Access HEX codes of colours that have been interpolated between those used in
+#' the CRUK logo.
+#'
+#' @param n The number of colours to return.
+#' @param colours The colours to interpolate between. By default these are set
+#'     to the blues from the UKRI logo.
+#' @param bias A positive number. Higher values will give more widely spaced
+#'     colours at the higher end of the colour spectrum.
+#' @param space A character string indicating whether the colours should be
+#'     interpolated in RGB or CIE Lab colour spaces.
+#' @param interpolate A character indicating whether a spline or linear
+#'     interpolation method should be used.
+#' @param alpha A logical value indicating whether an alpha channel should be
+#'     returned.
+#' @param ... Arguments to be passed to `grDevices::colorRampPalette`.
+#'
+#' @return A character vector of colours iterpolated from the blues in the UKRI
+#'     logo.
+#' @export
+#'
+#' @examples
+#'
+#' barplot(
+#'   1:5,
+#'   col = cruk_colours_n(5)
+#' )
+#'
+cruk_colours_n <- function(
+    n,
+    colours = cruk_colours(),
+    bias = 1,
+    space = "Lab",
+    interpolate = "linear",
+    alpha = FALSE,
+    ...
+) {
+  func_gen <- cruk_colours_palette_func_gen(
+    colors = colours,
+    bias = bias,
+    space = space,
+    interpolate = interpolate,
+    alpha = alpha
+  )
+  func_gen(n)
+}
